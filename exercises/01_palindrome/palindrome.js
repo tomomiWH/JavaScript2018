3b034e9b1c29a1619d7ac23f6976212a837a9778
Matina Patsos <mpatsos@deepbluecommunications.com>
2018-09-06 20:14:04 -0400

    Staring palindrome exercise

---
+++ exercises/01_palindrome/palindrome.js
@@ -0,0 +1,7 @@
+/**
+ * A palindrom is a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
+ * Checks to see if a string is a palindrome.
+ * @param  {[string]}  string
+ * @return {Boolean}
+ */
+function isPalindrome(string) {}

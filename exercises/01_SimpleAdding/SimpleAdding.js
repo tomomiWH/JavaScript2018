cfd3e0dee0dce5cbb59e90608b721d424df5caa7
Jamal Taylor <jamal.taylor@cengage.com>
2018-09-08 19:32:11 -0400

    second execrise

--- exercises/01_SimpleAdding/SimpleAdding.js
+++ exercises/01_SimpleAdding/SimpleAdding.js
@@ -5,6 +5,10 @@
  the parameter num will be any number from 1 to 1000. 
  */
 
-function SimpleAdding(num) {  
-  return num      
+function simpleAdding(num) {
+  return num;
 }
+
+module.exports = {
+  simpleAdding
+};

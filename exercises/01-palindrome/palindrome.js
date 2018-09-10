5120ef9afefe3fb9a0c97faa46e92a3d4a9cc3b2
matinaspatsos <matinaspatsos@gmail.com>
2018-09-10 19:10:19 -0400

    Update palindrome.js

--- exercises/01-palindrome/palindrome.js
+++ exercises/01-palindrome/palindrome.js
@@ -1,6 +1,6 @@
 /**
  * A palindrom is a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
- * Checks to see if a string is a palindrome.
+ * Checks to see if a string is a palindrome. You must remove special characters.
  * @param  {[string]}  string
  * @return {Boolean}
  */

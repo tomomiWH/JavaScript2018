b7af7ad71881dd932c91785ef36f0164e19adb56
Louis345 <jt125845@gmail.com>
2018-09-12 17:20:12 -0400

    fixed folder name and file name

--- test/02-largest-number-in-array/largest-number-in-array-test.js
+++ test/02-largest-number-in-array/largest-number-in-array-test.js
@@ -3,7 +3,7 @@ const chai = require('chai');
 const expect = chai.expect;
 const {
   largestNumberInArray
-} = require('../../exercises/02-largest-number- in-array/largest-number-in-array');
+} = require('../../exercises/02-largest-number-in-array/largest-number-in-array');
 describe('testing largest number in aray', () => {
   it('should return the largest number inside an array', () => {
     const biggestNumber = largestNumberInArray([10, 20, 30, -2]);

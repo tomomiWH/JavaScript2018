743114ac0003a07c97d2f2391c2fa0fff28069d6
Louis345 <jt125845@gmail.com>
2018-09-16 14:49:54 -0400

    added callback exercise

---
+++ exercises/03-callbacks/03-callbacks.js
@@ -0,0 +1,20 @@
+/*
+ * The printNames function should accept two arguments an array and a callback function named printer
+ * The second function named printer should accept one argument of type string and should prepend the string with three
+ * asterisks
+ * 
+ *
+ * 
+ * 
+ */
+
+var studentNames = ['Jamal', 'Matina', 'eddy'];
+
+function printer() {}
+
+function printNames() {}
+
+module.exports = {
+  printer,
+  printNames
+};

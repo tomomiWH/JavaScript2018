777ea674e3a0dd848294ea3397d146d60459ec86
Louis345 <jt125845@gmail.com>
2018-09-19 19:30:31 -0400

    Fixed an error in the test

--- test/04-map/04-map-test.js
+++ test/04-map/04-map-test.js
@@ -1,15 +1,15 @@
-const chai = require("chai");
+const chai = require('chai');
 const expect = chai.expect;
-chai.use(require("sinon-chai"));
-const assertArrays = require("chai-arrays");
+chai.use(require('sinon-chai'));
+const assertArrays = require('chai-arrays');
 chai.use(assertArrays);
 const {
   map,
   doubleValues,
   extractKey
-} = require("../../exercises/04-map/04-map");
+} = require('../../exercises/04-map/04-map');
 
-it("tests map to see if the function returns an array of modified values", () => {
+it('tests map to see if the function returns an array of modified values', () => {
   let suppliedArr;
   let arrIndex = [];
   const tripledValues = map([1, 2, 3], (number, index, array) => {
@@ -21,18 +21,16 @@ it("tests map to see if the function returns an array of modified values", () =>
   expect(arrIndex).to.be.equalTo([0, 1, 2]);
   expect(suppliedArr).to.be.equalTo([1, 2, 3]);
 });
-it("tests doubledValues to see if the function returns an array that doubling every value in the array", () => {
+it('tests doubledValues to see if the function returns an array that doubling every value in the array', () => {
   const doubledValues = doubleValues([2, 3, 4], number => {
     return number * 2;
   });
   expect(doubledValues).to.be.equalTo([4, 6, 8]);
 });
-it("tests extractedName to see if the function returns a new array with the value of a given key in each object", () => {
-  const extractedName = extractKey([
-    { name: "Elie" },
-    { name: "Tim" },
-    { name: "Matt" },
-    { name: "Colt" }
-  ]);
-  expect(extractedName).to.be.equalTo(["Elie", "Tim", "Matt", "Colt"]);
+it('tests extractedName to see if the function returns a new array with the value of a given key in each object', () => {
+  const extractedName = extractKey(
+    [{ name: 'Elie' }, { name: 'Tim' }, { name: 'Matt' }, { name: 'Colt' }],
+    'name'
+  );
+  expect(extractedName).to.be.equalTo(['Elie', 'Tim', 'Matt', 'Colt']);
 });

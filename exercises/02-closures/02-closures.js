61bf4ee16072e453dadac5039dd1c3a1f8c7d326
Matina Patsos <mpatsos@deepbluecommunications.com>
2018-09-22 16:59:18 -0400

    Updating .gitignore to exclude Visual Studio Code

--- exercises/02-closures/02-closures.js
+++ exercises/02-closures/02-closures.js
@@ -2,20 +2,12 @@
  * Create a function with a private variable called counter
  * The function should return two methods
  * It should return a function that accepts a parameter of type number. The function should add the nunber to counter
- * The second method should return another function to display the counter method
+ * The second method should return another function to display the counter variable
  *
  */
 
 function updateCounter() {
-  var counter = 0;
-  return {
-    add: function(num) {
-      counter += num;
-    },
-    retrieve: function() {
-      return counter;
-    }
-  };
+  return {};
 }
 
 var counterAPI = updateCounter();

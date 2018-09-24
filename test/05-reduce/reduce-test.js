8adb6599189ccff85869ec4adabe7844c6d29c8d
Matina Patsos <mpatsos@deepbluecommunications.com>
2018-09-24 17:52:31 -0400

    Fixing reduce test

--- test/05-reduce/reduce-test.js
+++ test/05-reduce/reduce-test.js
@@ -41,7 +41,7 @@ describe("testing the functionalitly of Reduce", () => {
     const flat = flattenArray([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
     expect(flat).to.be.equalTo([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
-  it("arrayToObject should transform and array of objects to accessible via id", () => {
+  it.only("arrayToObject should transform and array of objects to accessible via id", () => {
     const peopleArray = [
       { id: 123, name: "dave", age: 23 },
       { id: 456, name: "chris", age: 23 },
@@ -50,21 +50,13 @@ describe("testing the functionalitly of Reduce", () => {
       { id: 102, name: "tim", age: 23 }
     ];
     const peopleObject = {
-      "123": { id: 123, name: "dave", age: 23 },
-      "456": { id: 456, name: "chris", age: 23 },
-      "789": { id: 789, name: "bob", age: 23 },
-      "101": { id: 101, name: "tom", age: 23 },
-      "102": { id: 102, name: "tim", age: 23 }
+      123: { id: 123, name: "dave", age: 23 },
+      456: { id: 456, name: "chris", age: 23 },
+      789: { id: 789, name: "bob", age: 23 },
+      101: { id: 101, name: "tom", age: 23 },
+      102: { id: 102, name: "tim", age: 23 }
     };
     const arrOfObj = arrayToObject(peopleArray);
-    expect(arrOfObj).to.deep.equal({
-      "101": { id: 101, name: "tom", age: 23 },
-      "102": { id: 102, name: "tim", age: 23 },
-      "456": { id: 456, name: "chris", age: 23 },
-      "789": { id: 789, name: "bob", age: 23 },
-      id: 123,
-      name: "dave",
-      age: 23
-    });
+    expect(arrOfObj).to.deep.equal(peopleObject);
   });
 });

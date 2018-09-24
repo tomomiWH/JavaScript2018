70cc84e2c7ddafb202ac94e7f15a233008b2fd92
Matina Patsos <mpatsos@deepbluecommunications.com>
2018-09-23 20:14:33 -0400

    Adding ES6 classes exercises. Fixing arrow function unit tests. Adding more descriptive comments

--- exercises/05-arrow-functions/05-arrow-functions.js
+++ exercises/05-arrow-functions/05-arrow-functions.js
@@ -1,32 +1,47 @@
-/* 1 - Refactor the following code to use ES2015 arrow functions - make sure your function is also called add
-  
-  function add(a,b){
-    return a+ b;
-  }  
-*/
+/**
+ * Refactor the following to use ES6 arrow syntax.
+ * Make sure your refactored function is still called "add".
+ * Extra points if you can fit it on one line.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number} the sum of `a` plus `b`
+ */
+function add(a, b) {
+  return a + b;
+}
 
-/* 2 - Refactor the following code to use ES2015 arrow functions - make sure your function is also called tripleAndFilter
-
-    function tripleAndFilter(arr){
-      return arr.map(function(value){
-        return value * 3;
-      }).filter(function(value){
-        return value % 5 === 0;
-      })
-    }
-*/
-
-/* 3 - Refactor the following code to use ES2015 arrow functions. Make sure your function is also called doubleOddNumbers
-
-    function doubleOddNumbers(arr){
-        return arr.filter(function(val){
-            return val % 2 !== 0;
-        }).map(function(val){
-            return val *2;
-        })
-    }
-
-*/
+/**
+ * Refactor the following to use ES6 arrow syntax.
+ * Make sure your refactored function is still called "tripleAndFilter".
+ * @param {array} arr an array of numbers
+ * @returns {array} an array of only the numbers that, when multiplied by 3,
+ * are divisibile by 5
+ */
+function tripleAndFilter(arr) {
+  return arr
+    .map(function(value) {
+      return value * 3;
+    })
+    .filter(function(value) {
+      return value % 5 === 0;
+    });
+}
+/**
+ * Refactor the following to use ES6 arrow syntax.
+ * Make sure your refactored function is still called "doubleOddNumbers".
+ * @param {array} arr an array of numbers
+ * @returns {array} an array of only the even numbers. Each value in the
+ * new array of numbers is doubled
+ */
+function doubleOddNumbers(arr) {
+  return arr
+    .filter(function(val) {
+      return val % 2 !== 0;
+    })
+    .map(function(val) {
+      return val * 2;
+    });
+}
 
 module.exports = {
   add,

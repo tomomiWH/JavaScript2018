3e2ab2c90e4524f259f5b8742d3b47b96ebf2a4b
Matina Patsos <mpatsos@deepbluecommunications.com>
2018-09-30 20:49:38 -0400

    Adding event exercises

--- exercises/07-array-diff/07-array-diff.js
+++ exercises/07-array-diff/07-array-diff.js
@@ -1,20 +1,11 @@
-//Compare two arrays and return a new array with any items only found in one of the two given arrays,
+/**
+ * Compare two arrays and return a new array with only the values found in one of the arrays
+ * @param {array} arr1
+ * @param {array} arr2
+ * @returns {array} new array of unique values
+ */
+function diffArray(arr1, arr2) {}
 
-function diffArray(arr1, arr2) {
-  var newArr = [];
-  var newObj = {};
-
-  const mergedArr = arr1.concat(arr2);
-
-  mergedArr.forEach((item, idx) => {
-    newObj[item] ? (newObj[item] = false) : (newObj[item] = item);
-  });
-
-  Object.values(newObj).forEach((item, idx) => {
-    item && newArr.push(item);
-  });
-  return newArr;
-}
 module.exports = {
   diffArray
 };

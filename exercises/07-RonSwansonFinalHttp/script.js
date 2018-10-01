8823385dec16c2bbf2da19d84548905bba35e612
Louis345 <jt125845@gmail.com>
2018-10-01 17:37:06 -0400

    added instructions for the RonSwanson challenge

---
+++ exercises/07-RonSwansonFinalHttp/script.js
@@ -0,0 +1,7 @@
+/**
+ * use this to url to make your api calls: https://ron-swanson-quotes.herokuapp.com/v2/quotes'
+ *  As a user, I should be able to click on XHR and get a quote
+ *  As a user, I should be able to click on axios and get a quote
+ *  As a user, I should be able to click on FETCH and get a quote
+ *
+ */

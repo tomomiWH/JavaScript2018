70ea946500767986a3dc0e83c0e6582d14280470
Louis345 <jt125845@gmail.com>
2018-10-02 22:37:25 -0400

    added and fix challenges for Fetch

---
+++ exercises/07-Fetch/script.js
@@ -0,0 +1,22 @@
+/** An Example of the response you will receive when making an Http request
+ * https://dog.ceo/api/breeds/image/random
+ *
+ * {
+    "status": "success",
+    "message": "https://images.dog.ceo/breeds/poodle-toy/n02113624_9550.jpg"
+    }
+ *
+ * 
+ * 
+ */
+
+/**
+ * As a user, I should be able to click on the a button to see random dog images
+ * Please use Fetch in this Example
+ *
+ */
+
+const loadImage = url => {};
+
+/** This function should render an image to the page */
+const renderImageToPage = data => {};

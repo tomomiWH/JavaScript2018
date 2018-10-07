cddd6f8ae05f6ed8fb38ef2ebd70b29dd856b827
Matina Patsos <mpatsos@deepbluecommunications.com>
2018-10-07 19:52:26 -0400

    Adding first React exercises and examples

---
+++ react-exercises/09-fitness-tracker/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import FitnessTracker from "./FitnessTracker";
+
+ReactDOM.render(<FitnessTracker />, document.getElementById("root"));

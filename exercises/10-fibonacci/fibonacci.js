63bac4d5638cf386bb966c883b34962051b60b15
shigeru <Shigeru Imai>
2018-10-14 00:54:50 -0400

    Add exercise and test for Fibonacci

---
+++ exercises/10-fibonacci/fibonacci.js
@@ -0,0 +1,16 @@
+/**
+ * A function that accepts an integer n and returns the n-th Fibonacci number, fib(n).
+ * Given fib(0) = 0 and fib(1) = 1, fib(n) is recursively defined as follows:
+ *   fib(n) = fib(n-1) + fib(n-2)
+ *
+ * @param  {integer} n
+ * @return {integer} fib(n)
+ */
+
+function fib(n) {
+    return
+}
+
+module.exports = {
+    fib
+};
